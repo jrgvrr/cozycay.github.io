1c5434aae5fc0a5a6cc1a510bef3b39b570a373d
jrvmdt <omadto.jervi65@gmail.com>
2023-03-28 18:13:39 +0800

    Added sign up

--- js/psswrd.js
+++ js/psswrd.js
@@ -1,20 +1,23 @@
-
 function ConfirmPass() {
-    let ekiska = document.getElementById('ekis1')
-    let circle = document.getElementById('circle1');
-    let pass1 = document.getElementById('turn1').value;
-    let pass2 = document.getElementById('turn2').value;
-   
-   
-    if (pass2 === pass1 || pass1==pass2) {
-         circle.style.visibility = "visible"
-         ekiska.style.visibility = "hidden"
-        
-    }
-    
-    else if (pass1 !==pass2 || pass2 !==pass1){
-        circle.style.visibility = "hidden"
-        ekiska.style.visibility = "visible"
-        
-    }
-    }
+  let ekiska = document.getElementById("ekis1");
+  let circle = document.getElementById("circle1");
+  let pass1 = document.getElementById("turn1").value;
+  let pass2 = document.getElementById("turn2").value;
+
+  if (pass1 == null || pass1 == "" || pass2 == null || pass2 == "") {
+    circle.style.visibility = "hidden";
+    ekiska.style.display = "flex";
+    ekiska.style.visibility = "visible";
+    ekiska.style.position = "absolute";
+    ekiska.style.left = "520px";
+  } else if (pass2 === pass1 || pass1 === pass2) {
+    circle.style.visibility = "visible";
+    ekiska.style.visibility = "hidden";
+  } else if (pass1 !== pass2 || pass2 !== pass1) {
+    circle.style.visibility = "hidden";
+    ekiska.style.display = "flex";
+    ekiska.style.visibility = "visible";
+    ekiska.style.position = "absolute";
+    ekiska.style.left = "520px";
+  }
+}
